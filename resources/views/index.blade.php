c6ece88a2a99b890beeedd690d1f8d63bd7453f3
Mouad Benali <mouadbnl.2k01@gmail.com>
2022-03-05 16:13:38 +0100

    responsive landing page

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,5 +1,5 @@
 <x-layouts.base-layout>
-    <section class="py-24 bg-dark text-white">
+    <section class="py-12 md:py-24 bg-dark text-white">
         <div class="container">
             <div class="pt-8 grid grid-cols-2 gap-x-8 mb-24">
                 <div class="flex items-center">
@@ -19,7 +19,7 @@
                     </article>
                 </div>
 
-                <div class="pl-24">
+                <div class="lg:pl-24">
                     <div class="ratio-720 bg-gray-300 relative overflow-hidden">
                         <img class="absolute inset-0 object-fill object-center" src="{{ $latestPost->featured_image ?? 'https://dummyimage.com/16:9x720&text=No+Image' }}" alt="image">
                     </div>
@@ -27,7 +27,7 @@
             </div>
 
 
-            <div class="grid grid-cols-4 gap-8">
+            <div class="hidden md:grid grid-cols-2 md:grid-cols-2 lg:grid-cols-4 gap-8">
                 @foreach ($posts->take(4) as $post)
                     <x-post-card :post="$post" />
                 @endforeach
@@ -39,7 +39,7 @@
     <section class="py-24">
         <div class="container">
             <h2 class="text-primary mb-16">Latest articles</h2>
-            <div class="grid grid-cols-4 gap-x-8 gap-y-16">
+            <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-x-8 gap-y-16">
                 @foreach ($posts->take(16) as $post)
                     <x-post-card :post="$post" />
                 @endforeach
