97c3850113bd5611523ec909ee558a669a785535
Mouad Benali <mouadbnl.2k01@gmail.com>
2022-03-05 15:32:12 +0100

    setup the landing page

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,7 +1,63 @@
 <x-layouts.base-layout>
-    @foreach ($posts as $post)
-        <a href="{{ route('posts.show', $post->slug) }}">{{ $post->title }}</a>,
-        {{ $post->created_at->diffForHumans() }},
-        {{ $post->author->name }} <br>
-    @endforeach
+    <section class="py-24 bg-dark text-white">
+        <div class="container">
+            <div class="pt-8 grid grid-cols-2 gap-x-8 mb-24">
+                <div class="flex items-center">
+                    <article>
+                        <div class="flex space-x-2 mb-4">
+                            <span class="block px-4 py-1 rounded-full transition bg-primary text-white bg-opacity-100 hover:bg-opacity-90">news</span>
+                            <span class="block px-4 py-1 rounded-full transition bg-primary text-white bg-opacity-100 hover:bg-opacity-90">contest</span>
+                            <span class="block px-4 py-1 rounded-full transition bg-primary text-white bg-opacity-100 hover:bg-opacity-90">competitive programming</span>
+                            <span class="block px-4 py-1 rounded-full transition bg-primary text-white bg-opacity-100 hover:bg-opacity-90">ensak</span>
+                        </div>
+                        <h2>Managing Secrets in Laravel with AWS Parameter Store</h2>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vitae non et in tristique aliquam. A eget euismod arcu et. Id ultricies ac sem sagittis. Amet sagittis, faucibus in ut mauris, ligula. Interdum non.
+                        </p>
+                    </article>
+                </div>
+
+                <div class="pl-24">
+                    <div class="ratio-720 bg-gray-300">
+
+                    </div>
+                </div>
+            </div>
+
+
+            <div class="grid grid-cols-4 gap-8">
+                @for ($i = 0; $i < 4; $i++)
+                    <article>
+                        <div class="ratio-720 bg-gray-300 mb-3"></div>
+                        <div class="flex space-x-2 mb-3 text-sm">
+                            <span class="block px-4 py-1 rounded-full transition bg-primary text-white bg-opacity-100 hover:bg-opacity-90">news</span>
+                            <span class="block px-4 py-1 rounded-full transition bg-primary text-white bg-opacity-100 hover:bg-opacity-90">contest</span>
+                            <span class="block px-4 py-1 opacity-70">December 1st, 2021</span>
+                        </div>
+                        <h3>Most popular IT certifications for 2022</h3>
+                    </article>
+                @endfor
+            </div>
+        </div>
+    </section>
+
+
+    <section class="py-24">
+        <div class="container">
+            <h2 class="text-primary mb-16">Latest articles</h2>
+            <div class="grid grid-cols-4 gap-x-8 gap-y-16">
+                @for ($i = 0; $i < 16; $i++)
+                    <article>
+                        <div class="ratio-720 bg-gray-300 mb-3"></div>
+                        <div class="flex flex-wrap gap-2 mb-3 text-sm">
+                            <span class="block px-4 py-1 rounded-full transition bg-primary text-white bg-opacity-100 hover:bg-opacity-90">news</span>
+                            <span class="block px-4 py-1 rounded-full transition bg-primary text-white bg-opacity-100 hover:bg-opacity-90">contest</span>
+                            <span class="block py-1 opacity-70">December 1st, 2021</span>
+                        </div>
+                        <h3>Most popular IT certifications for 2022</h3>
+                    </article>
+                @endfor
+            </div>
+        </div>
+    </section>
 </x-layouts.base-layout>
--- resources/views/layouts/base-layout.blade.php
+++ resources/views/layouts/base-layout.blade.php
@@ -7,37 +7,92 @@
         <title>Laravel</title>
 
         <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
 
         <!-- Styles -->
         <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 
         <style>
             body {
-                font-family: 'Nunito', sans-serif;
+                font-family: 'Poppins', sans-serif;
             }
         </style>
     </head>
     <body class="antialiased">
-        <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
-            @if (Route::has('login'))
-                <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
-                    @auth
-                        <a href="{{ url('/home') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Home</a>
-                    @else
-                        <a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Log in</a>
 
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Register</a>
-                        @endif
-                    @endauth
+        <header class="bg-dark text-light">
+            <div class="container flex items-center justify-between py-4">
+                <h1 class="text-2xl font-extrabold">EIC Blog</h1>
+
+                <nav>
+                    <ul class="flex space-x-2">
+                        <li><a href="#" class="px-6 py-2 transition bg-white bg-opacity-0 hover:bg-opacity-10">Blog</a></li>
+                        <li><a href="#" class="px-6 py-2 transition bg-white bg-opacity-0 hover:bg-opacity-10">EIC Talks</a></li>
+                        <li><a href="#" class="px-6 py-2 transition bg-white bg-opacity-0 hover:bg-opacity-10">Projects</a></li>
+                        <li><a href="#" class="px-6 py-2 transition bg-white bg-opacity-0 hover:bg-opacity-10">About the club</a></li>
+                    </ul>
+                </nav>
+            </div>
+        </header>
+
+        
+
+        <main>
+            {{ $slot }}
+        </main>
+
+        <footer class="bg-dark py-16 text-light">
+            <div class="container flex items-center justify-between mb-16">
+                <div class="">
+                    <h4 class="text-2xl font-medium mb-4">Follow EIC on</h4>
+                    <div class="grid grid-cols-4 gap-4 text-sm">
+                        <a href="#" class="bg-white bg-opacity-10 p-2 flex space-x-4 items-center transition hover:bg-opacity-25">
+                            <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M17.7188 9C17.7188 4.18359 13.8164 0.28125 9 0.28125C4.18359 0.28125 0.28125 4.18359 0.28125 9C0.28125 13.3516 3.46957 16.9587 7.6377 17.6133V11.5204H5.42285V9H7.6377V7.07906C7.6377 4.8941 8.93848 3.68719 10.9308 3.68719C11.8849 3.68719 12.8827 3.85734 12.8827 3.85734V6.00187H11.783C10.7002 6.00187 10.3623 6.67406 10.3623 7.36348V9H12.7804L12.3936 11.5204H10.3623V17.6133C14.5304 16.9587 17.7188 13.3516 17.7188 9Z" fill="#FAFAFA"/>
+                            </svg>
+                            <span>Facebook</span>                                
+                        </a>
+                        <a href="#" class="bg-white bg-opacity-10 p-2 flex space-x-4 items-center transition hover:bg-opacity-25">
+                            <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M17.7188 9C17.7188 4.18359 13.8164 0.28125 9 0.28125C4.18359 0.28125 0.28125 4.18359 0.28125 9C0.28125 13.3516 3.46957 16.9587 7.6377 17.6133V11.5204H5.42285V9H7.6377V7.07906C7.6377 4.8941 8.93848 3.68719 10.9308 3.68719C11.8849 3.68719 12.8827 3.85734 12.8827 3.85734V6.00187H11.783C10.7002 6.00187 10.3623 6.67406 10.3623 7.36348V9H12.7804L12.3936 11.5204H10.3623V17.6133C14.5304 16.9587 17.7188 13.3516 17.7188 9Z" fill="#FAFAFA"/>
+                            </svg>
+                            <span>Facebook</span>                                
+                        </a>
+                        <a href="#" class="bg-white bg-opacity-10 p-2 flex space-x-4 items-center transition hover:bg-opacity-25">
+                            <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M17.7188 9C17.7188 4.18359 13.8164 0.28125 9 0.28125C4.18359 0.28125 0.28125 4.18359 0.28125 9C0.28125 13.3516 3.46957 16.9587 7.6377 17.6133V11.5204H5.42285V9H7.6377V7.07906C7.6377 4.8941 8.93848 3.68719 10.9308 3.68719C11.8849 3.68719 12.8827 3.85734 12.8827 3.85734V6.00187H11.783C10.7002 6.00187 10.3623 6.67406 10.3623 7.36348V9H12.7804L12.3936 11.5204H10.3623V17.6133C14.5304 16.9587 17.7188 13.3516 17.7188 9Z" fill="#FAFAFA"/>
+                            </svg>
+                            <span>Facebook</span>                                
+                        </a>
+                        <a href="#" class="bg-white bg-opacity-10 p-2 flex space-x-4 items-center transition hover:bg-opacity-25">
+                            <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M17.7188 9C17.7188 4.18359 13.8164 0.28125 9 0.28125C4.18359 0.28125 0.28125 4.18359 0.28125 9C0.28125 13.3516 3.46957 16.9587 7.6377 17.6133V11.5204H5.42285V9H7.6377V7.07906C7.6377 4.8941 8.93848 3.68719 10.9308 3.68719C11.8849 3.68719 12.8827 3.85734 12.8827 3.85734V6.00187H11.783C10.7002 6.00187 10.3623 6.67406 10.3623 7.36348V9H12.7804L12.3936 11.5204H10.3623V17.6133C14.5304 16.9587 17.7188 13.3516 17.7188 9Z" fill="#FAFAFA"/>
+                            </svg>
+                            <span>Facebook</span>                                
+                        </a>
+                    </div>
                 </div>
-            @endif
 
-            <main>
-                {{ $slot }}
-            </main>
+                <div class="h-24 w-80 bg-white bg-opacity-20">
 
-        </div>
+                </div>
+            </div>
+
+            <div class="container flex items-center justify-between">
+                <div class="flex space-x-4 text-sm">
+                    <a href="#" class="hover:underline">Blog</a>
+                    <a href="#" class="hover:underline">Blog</a>
+                    <a href="#" class="hover:underline">Blog</a>
+                    <a href="#" class="hover:underline">Blog</a>
+                    <a href="#" class="hover:underline">Blog</a>
+                </div>
+
+                <div class="text-xs">
+                    <span>made with ❤️ by Mouad Benali, © 2021 - 2022 EIC</span>
+                </div>
+            </div>
+        </footer>
+
     </body>
 </html>
